2399cc42977560fb9f52023e171652edce24708d
B Horn <b@horn.uk>
2024-01-16 00:03:38 +0000

    Make selftest.sh run the script again
    Signed-off-by: B Horn <b@horn.uk>

--- selftest.sh
+++ selftest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 RES=`find ./threadsnake/ -depth 1 -name \*.py | xargs python3 ./threadsnake/`
-#echo "$RES" | wc
-echo "$RES" #| python3 - ./tests/test2.py
+echo "$RES" | wc
+echo "$RES" | python3 - ./tests/test2.py
